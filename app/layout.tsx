af3fce3a7e2b2b19f8980464aecf44e947be4ace
hakut <hakut@example.com>
2026-01-18 00:05:51 +0900

    feat: integrate GA4 tracking G-J0S6KYW409 and optimize layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,28 @@ export default function RootLayout({
                         }}
                     />
                 )}
+                {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+                    <>
+                        <Script
+                            strategy="afterInteractive"
+                            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+                        />
+                        <Script
+                            id="ga-script"
+                            strategy="afterInteractive"
+                            dangerouslySetInnerHTML={{
+                                __html: `
+                                    window.dataLayer = window.dataLayer || [];
+                                    function gtag(){dataLayer.push(arguments);}
+                                    gtag('js', new Date());
+                                    gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                                        page_path: window.location.pathname,
+                                    });
+                                `,
+                            }}
+                        />
+                    </>
+                )}
             </head>
             <body className={inter.className}>
                 {process.env.NEXT_PUBLIC_GTM_ID && (
@@ -60,6 +82,6 @@ export default function RootLayout({
                 <InstallBanner />
                 <Footer />
             </body>
-        </html>
+        </html >
     )
 }
