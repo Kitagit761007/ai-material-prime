3eb6da4062eee553ba8c028e6ba95f0abfa5f070
Kitagit761007 <137395887+Kitagit761007@users.noreply.github.com>
2026-02-02 20:12:42 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        formats: ['image/webp', 'image/avif'],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-            },
-        ],
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+  // 1. GitHub Pages用にHTMLとして書き出す設定
+  output: 'export',
+  
+  // 2. GitHub Pagesで画像を表示させるための設定
+  images: {
+    unoptimized: true,
+  },
+  
+  // 3. リンク（/category/GXなど）を正常に動かすための設定
+  trailingSlash: true,
 };
 
 export default nextConfig;
