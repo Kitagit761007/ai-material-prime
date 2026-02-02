73ef5e2704de86006a775ad759130062f9fd9e53
Kitagit761007 <137395887+Kitagit761007@users.noreply.github.com>
2026-02-02 20:27:20 +0900

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',           // 静的HTMLとして書き出す（必須）
-  images: { unoptimized: true }, // 画像を表示させる（必須）
-  trailingSlash: true,        // URLの末尾を整えて404を防ぐ（必須）
-};
-
-module.exports = nextConfig;
